e56dd9df01f264eeba81f869083a7a4ed23e9e3d
davidcoats <david@getsafetycone.com>
2022-11-13 22:33:10 -0800

    Initial commit.

---
+++ source/R5T.E0061.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.E0061.Construction
+Construction console project for the R5T.E0061 library.
---
+++ source/R5T.E0061/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.E0061
+{
+	/// <summary>
+	/// NuGet related NuGet packages experiments.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
